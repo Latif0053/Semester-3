6eda8e2b634fba3ec85b04722a73ee62f5aadfd3
Latif0053 <aliefmuhammadlatif@gmail.com>
2025-09-07 23:03:54 +0700

    Add files via upload

---
+++ Praktikum Pemrograman Perangkat Bergerak/pertemuan 3/MyPhonealiefConvertToConstraint-master/MyPhone/app/src/main/java/com/example/myphone/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myphone
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val mainView: View? = findViewById(R.id.main)
+        if (mainView != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(mainView) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        } else {
+            // Log an error or handle the case where the view is not found.
+            // This is crucial for debugging.
+            Log.e("MainActivity", "View with ID 'main' not found in R.layout.activity_main. Ensure it exists in your XML.")
+        }
+    }
+}
